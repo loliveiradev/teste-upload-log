e875ede1c8382095e66d5caf5def154c7a118b44
Lucas Oliveira <loliveira.development@gmail.com>
2020-04-24 14:01:23 -0300

    first commit

---
+++ upload-log/src/main/java/com/lucasoliveira/uploadlog/UploadLogApplication.java
@@ -0,0 +1,13 @@
+package com.lucasoliveira.uploadlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadLogApplication.class, args);
+	}
+
+}
